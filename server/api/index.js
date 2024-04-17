c591bd7e5f53e12f278468f17753439a25ad83ea
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-04-17 22:54:44 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: "*"
 }))
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://aiquill.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     res.header(
         "Access-Control-Allow-Headers",
