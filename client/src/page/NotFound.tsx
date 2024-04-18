43c16348c06b1430579d2c52417fe313ff6decde
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-04-18 13:40:23 +0530

    devpods
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- client/src/page/NotFound.tsx
+++ client/src/page/NotFound.tsx
@@ -3,11 +3,23 @@ import { Link } from 'react-router-dom'
 
 function NotFound() {
     return (
-        <div>
-            <h2>Nothing to see here!</h2>
+        <div className='grid place-content-center h-screen bg-dot-thick-red-800'>
+            <h2>Landed Somewhere Unkown!</h2>
             <p>
                 <Link to="/">Go to the home page</Link>
             </p>
+            <div className='flex flex-row gap-10 justify-center items-center'>
+                <a href="/explain" >
+                    <button className="bg-green-500  rounded-lg pt-3 pb-3 pr-8 pl-8">
+                        Code Explain
+                    </button>
+                </a>
+                <a href="/review" >
+                    <button className="bg-green-500  rounded-lg pt-3 pb-3 pr-8 pl-8">
+                        Code Review
+                    </button>
+                </a>
+            </div>
         </div>
     )
 }
