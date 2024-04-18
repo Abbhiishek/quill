acec76a0e84aafc9c9e355a940de098f8d404704
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-04-18 12:45:32 +0530

    fix
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/"}>
+    <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
       </ClerkProvider>
